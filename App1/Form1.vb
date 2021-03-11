5e8ae5baf726acf725e98585fc9e5ef62a3529fe
MrCoult <80452894+MrCoult@users.noreply.github.com>
2021-03-11 13:04:23 +0200

    Add files via upload

---
+++ App1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox3.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox3.Text = Val(TextBox1.Text) - Val(TextBox2.Text)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox3.Text = Val(TextBox1.Text) * Val(TextBox2.Text)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox3.Text = Format(Val(TextBox1.Text) / Val(TextBox2.Text), "0.00")
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox3.Text = TextBox1.Text + TextBox2.Text
+
+    End Sub
+End Class
